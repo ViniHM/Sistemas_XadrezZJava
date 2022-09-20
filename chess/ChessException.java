187af718a8c8b9c98855e2f95cc56fda31d68ba6
ViniHM <viniciushenriquemedeiros.95@gmail.com>
2022-09-20 14:57:32 -0300

    Moving pieces

--- chess/ChessException.java
+++ chess/ChessException.java
@@ -1,4 +1,4 @@
-public class ChessException extends RuntimeException {
+public class ChessException extends BoardException {
     // private static final long serialVersionUID = 1L
 
     public ChessException(String msg){
